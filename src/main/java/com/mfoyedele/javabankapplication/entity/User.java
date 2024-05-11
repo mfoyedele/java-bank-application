c18d4bdbc6b66b6c77ebb06da731e45f647ee8a4
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-11 23:04:39 +0100

    User's entities added to entity package

--- src/main/java/com/mfoyedele/javabankapplication/entity/User.java
+++ src/main/java/com/mfoyedele/javabankapplication/entity/User.java
@@ -1,4 +1,20 @@
 package com.mfoyedele.javabankapplication.entity;
 
+import java.math.BigDecimal;
+
 public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    private String email;
+    private String phoneNumber;
+    private String alternatePhoneNumber;
+    private String status;
+
 }
