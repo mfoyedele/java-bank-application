a076c68c73f8f8f717d2cbd4bef48c5dc8e085bc
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 15:50:53 +0100

    Update accountUtils

--- src/main/java/com/mfoyedele/javabankapplication/utils/AccountUtils.java
+++ src/main/java/com/mfoyedele/javabankapplication/utils/AccountUtils.java
@@ -1,5 +1,18 @@
 package com.mfoyedele.javabankapplication.utils;
 
+import java.time.Year;
+
 public class AccountUtils {
 
+    /**
+     * 2004 + randomSixDigits
+     */
+
+    Year currentYear = Year.now();
+    int min = 100000;
+    int max = 900000;
+
+    // Generate a random number between min and max
+    int randomNumber = (int) Math.floor(Math.random() * (max - min + 1) +  min);
+    // Convert the current year and randomNumber to strings, then concatenate them.
 }
