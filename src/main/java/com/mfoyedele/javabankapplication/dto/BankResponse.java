8d6ac44ddd8db09e9b41c0051581d581a783792c
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 15:10:48 +0100

    BankResponse dto created

---
+++ src/main/java/com/mfoyedele/javabankapplication/dto/BankResponse.java
@@ -0,0 +1,7 @@
+package com.mfoyedele.javabankapplication.dto;
+
+public class BankResponse {
+    private String responseCode;
+    private String responseMessage;
+    private AccountInfo accountInfo;
+}
