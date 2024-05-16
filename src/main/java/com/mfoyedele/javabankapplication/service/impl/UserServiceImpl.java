4477dba1084d6f520c93ca94ca28b6ae1d838452
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 15:26:34 +0100

    UserServiceImpl created

---
+++ src/main/java/com/mfoyedele/javabankapplication/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.mfoyedele.javabankapplication.service.impl;
+
+import com.mfoyedele.javabankapplication.dto.BankResponse;
+import com.mfoyedele.javabankapplication.dto.UserRequest;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public BankResponse createAccount(UserRequest userRequest) {
+
+    }
+}
