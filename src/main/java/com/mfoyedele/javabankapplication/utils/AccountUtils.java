ae9164a668c64bb7306501ad26308ebb8d55dd03
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 16:02:03 +0100

    Update accountUtils

--- src/main/java/com/mfoyedele/javabankapplication/utils/AccountUtils.java
+++ src/main/java/com/mfoyedele/javabankapplication/utils/AccountUtils.java
@@ -4,19 +4,23 @@ import java.time.Year;
 
 public class AccountUtils {
 
-    /**
-     * 2004 + randomSixDigits
-     */
+    public static String generateAccountNumber() {
+        /**
+         * 2004 + randomSixDigits
+         */
 
-    Year currentYear = Year.now();
-    int min = 100000;
-    int max = 900000;
+        Year currentYear = Year.now();
+        int min = 100000;
+        int max = 900000;
 
-    // Generate a random number between min and max
-    int randNumber = (int) Math.floor(Math.random() * (max - min + 1) +  min);
+        // Generate a random number between min and max
+        int randNumber = (int) Math.floor(Math.random() * (max - min + 1) +  min);
 
-    // Convert the current year and randomNumber to strings, then concatenate them.
-    String year = String.valueOf(currentYear);
-    String randomNumber = String.valueOf(randNumber);
-    StringBuilder accountNumber = new StringBuilder();
+        // Convert the current year and randomNumber to strings, then concatenate them.
+        String year = String.valueOf(currentYear);
+        String randomNumber = String.valueOf(randNumber);
+        StringBuilder accountNumber = new StringBuilder();
+
+        return accountNumber.append(year).append(randomNumber).toString();
+    }
 }
