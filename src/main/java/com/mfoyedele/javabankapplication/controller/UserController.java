1612ebacdd849adeea40abfc6eb9f7ff98a613da
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 17:04:23 +0100

    Update UserController

--- src/main/java/com/mfoyedele/javabankapplication/controller/UserController.java
+++ src/main/java/com/mfoyedele/javabankapplication/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.mfoyedele.javabankapplication.controller;
 
+import com.mfoyedele.javabankapplication.dto.BankResponse;
+import com.mfoyedele.javabankapplication.dto.UserRequest;
+import com.mfoyedele.javabankapplication.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public BankResponse createAccount(@RequestBody UserRequest userRequest) {
+        return userService.createAccount(userRequest);
+    }
 }
