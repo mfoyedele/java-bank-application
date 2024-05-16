2eedd072b3f326d6546846236dee7cf64218a594
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 15:33:15 +0100

    Update userServiceImpl

--- src/main/java/com/mfoyedele/javabankapplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/mfoyedele/javabankapplication/service/impl/UserServiceImpl.java
@@ -2,11 +2,20 @@ package com.mfoyedele.javabankapplication.service.impl;
 
 import com.mfoyedele.javabankapplication.dto.BankResponse;
 import com.mfoyedele.javabankapplication.dto.UserRequest;
+import com.mfoyedele.javabankapplication.entity.User;
 
 public class UserServiceImpl implements UserService {
 
     @Override
     public BankResponse createAccount(UserRequest userRequest) {
+        /**
+         * Creating an account -
+         */
+        User newUser = User.builder()
+                .firstName(userRequest.getFirstName())
+                .lastName(userRequest.getLastName())
+                .otherName(userRequest.getOtherName())
 
+                .build();
     }
 }
