840deadc8f9eb2a32ea4f060f78a8ccddc532b35
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 15:14:21 +0100

    AccountInfo dto created

---
+++ src/main/java/com/mfoyedele/javabankapplication/dto/AccountInfo.java
@@ -0,0 +1,7 @@
+package com.mfoyedele.javabankapplication.dto;
+
+public class AccountInfo {
+    private String accountName;
+    private String accountBalance;
+    private String accountNumber;
+}
