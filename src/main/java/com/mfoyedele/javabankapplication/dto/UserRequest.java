502fb671a4d4ae25dae8e379cfdbd9e4df189bfa
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 15:04:50 +0100

    UserRequest dto created

---
+++ src/main/java/com/mfoyedele/javabankapplication/dto/UserRequest.java
@@ -0,0 +1,16 @@
+package com.mfoyedele.javabankapplication.dto;
+
+import java.math.BigDecimal;
+
+public class UserRequest {
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String email;
+    private String phoneNumber;
+    private String alternatePhoneNumber;
+    private String status;
+}
