2aace2ea99612e49548d627e35938a0c87062084
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-16 15:16:29 +0100

    Update accountInfo dto

--- src/main/java/com/mfoyedele/javabankapplication/dto/AccountInfo.java
+++ src/main/java/com/mfoyedele/javabankapplication/dto/AccountInfo.java
@@ -1,5 +1,14 @@
 package com.mfoyedele.javabankapplication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountInfo {
     private String accountName;
     private String accountBalance;
