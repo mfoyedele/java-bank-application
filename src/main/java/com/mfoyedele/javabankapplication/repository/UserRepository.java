f6cc31b7adfaaa4d3998f3b12fb0b59cf78ebd21
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-17 16:22:58 +0100

    Update UserRepository

--- src/main/java/com/mfoyedele/javabankapplication/repository/UserRepository.java
+++ src/main/java/com/mfoyedele/javabankapplication/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.mfoyedele.javabankapplication.repository;
 import com.mfoyedele.javabankapplication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
 }
