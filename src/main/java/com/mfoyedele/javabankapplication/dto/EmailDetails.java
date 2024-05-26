99fd97fa197cc99bf03d48f40fd6045fc429fa6d
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-26 08:14:46 +0100

    Update emailDetails

--- src/main/java/com/mfoyedele/javabankapplication/dto/EmailDetails.java
+++ src/main/java/com/mfoyedele/javabankapplication/dto/EmailDetails.java
@@ -1,5 +1,14 @@
 package com.mfoyedele.javabankapplication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EmailDetails {
     private String recipient;
     private String subject;
