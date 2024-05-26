87c35333977b688f47d19b0194d9c93ae25c7be4
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-26 08:18:21 +0100

    Update emailDetails

---
+++ src/main/java/com/mfoyedele/javabankapplication/service/impl/EmailService.java
@@ -0,0 +1,9 @@
+package com.mfoyedele.javabankapplication.service.impl;
+
+import com.mfoyedele.javabankapplication.dto.EmailDetails;
+
+public class EmailService {
+    void sendEmailAlert(EmailDetails emailDetails) {
+
+    }
+}
