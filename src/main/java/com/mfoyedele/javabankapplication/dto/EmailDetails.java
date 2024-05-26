eb9e896b3dd89cfb283cb28493dd2f78f182a07b
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-26 08:12:04 +0100

    Update emailDetails

--- src/main/java/com/mfoyedele/javabankapplication/dto/EmailDetails.java
+++ src/main/java/com/mfoyedele/javabankapplication/dto/EmailDetails.java
@@ -1,4 +1,8 @@
 package com.mfoyedele.javabankapplication.dto;
 
 public class EmailDetails {
+    private String recipient;
+    private String subject;
+    private String messageBody;
+    private String attachment;
 }
