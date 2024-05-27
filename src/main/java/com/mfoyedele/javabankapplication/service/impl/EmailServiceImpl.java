e42ee318adb84069145dc3e142392df486664181
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-27 01:00:09 +0100

    Update EmailServiceImpl

--- src/main/java/com/mfoyedele/javabankapplication/service/impl/EmailServiceImpl.java
+++ src/main/java/com/mfoyedele/javabankapplication/service/impl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package com.mfoyedele.javabankapplication.service.impl;
 import com.mfoyedele.javabankapplication.dto.EmailDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,11 @@ public class EmailServiceImpl extends EmailService {
             mailMessage.setTo(emailDetails.getRecipient());
             mailMessage.setText(emailDetails.getMessageBody());
             mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail sent successfully");
+        } catch (MailException e) {
+            throw new RuntimeException(e);
         }
 
     }
